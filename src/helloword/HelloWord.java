4429689d25d3ad9c83e7a7c19a9f343cf4944db1
小贺 <@DESKTOP-9RFDRTO>
2020-03-27 20:36:10 +0800

    text

--- src/helloword/HelloWord.java
+++ src/helloword/HelloWord.java
@@ -3,6 +3,7 @@ package helloword;
 public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("helloword");
-		System.out.println("µÚ¶þ´Î²âÊÔ");
+		
+		System.out.println("çš„æ’’");
 	}
 }
